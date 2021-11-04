a00031f93dc7a9a0200e467f4ca6fa4953478d31
Irvine <91871288+improve-d@users.noreply.github.com>
2021-11-04 20:28:27 +0800

    Delete schemas directory

--- schemas/blacklist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const blacklistSchema = new mongoose.Schema({
-    userId: String,
-    username: String
-})
-
-module.exports = mongoose.model('Blacklist', blacklistSchema)
